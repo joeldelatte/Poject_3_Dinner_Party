dbbce261583e173c858acd03092f09dd6bfa5dc2
Joel Delatte <joeldelatte@gmail.com>
2020-03-18 22:29:43 -0500

    adds invitation api route, config folder, index.js in models folder, and schema drop and create

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const invitationRoutes = require("./invitation");
 // this is copied from 21.5 may need adjustments
 router.use("/invitation", invitationRoutes);
+// how do I add a other routes? possible for Users?
+router.use("")
 
 module.exports = router;
