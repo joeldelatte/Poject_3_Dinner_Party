ea2e81b2966857159459e8ad29e7e05210691940
Joel Delatte <joeldelatte@gmail.com>
2020-04-02 00:05:39 -0500

    logs UserId of event

--- client/src/components/Feeds/Feeds.js
+++ client/src/components/Feeds/Feeds.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Feed from './Feed/Feed';
+import API from "../../utils/API";
+
 
 export default function Feeds(props) {
 
     function click(event) {
-        console.log(event);
+        console.log(event.UserId);
+
     }
     return props.events.map((event, index) => {
         return (
