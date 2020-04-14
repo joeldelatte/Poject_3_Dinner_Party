1ab2160eb71cf7dff12898a0f5e9295ecdfc1b1d
Tad83 <tad83hemingway@gmail.com>
2020-04-14 16:23:49 -0500

    Got the page title to work

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,43 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-export default function navbar(props) {
+export default function Navbar(props) {
 
-    // const [pageTitle, setPageTitle] = useState([]);
+    const [pageTitle, setPageTitle] = useState("");
 
-    // useEffect(() => {
-    //     updateTitle()
-    // }, [])
+    useEffect(() => {
+        updateTitle()
+    }, [])
 
-    // function updateTitle() {
-    //     if (window.location.pathname = '/events') {
-    //         setPageTitle('Current Events')
-    //     }
-    //     if (window.location.pathname = '/dashboard') {
-    //         setPageTitle('User Dashboard')
-    //     }
-    //     if (window.location.pathname = '/create') {
-    //         setPageTitle('Create Event')
-    //     }
-    // }
-
-    // function updateTitle() {
-    //     if (window.location.pathname = '/events') {
-    //         return ('Current Events')
-    //     }
-    //     if (window.location.pathname = '/dashboard') {
-    //         return ('User Dashboard')
-    //     }
-    //     if (window.location.pathname = '/create') {
-    //         return ('Create Event')
-    //     }
-    // }
+    function updateTitle() {
+        switch (window.location.pathname) {
+            case '/events':
+                setPageTitle('Current Events');
+                break;
+            case '/dashboard':
+                setPageTitle('User Dashboard');
+                break;
+            case '/create':
+                setPageTitle('Create Event');
+                break;
+            default:
+                setPageTitle('');
+                break;
+        }
+    }
 
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-fixed-top">
                 <Link className='loggin-user' to='/dashboard'>{props.currentUser}</Link>
-                <div className='page-title nav navbar-nav navbar-center'>Testing</div>
+                <div className='page-title nav navbar-nav navbar-center'>{pageTitle}</div>
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav ml-auto nav-padding">
                         <li className="nav-item dropdown">
