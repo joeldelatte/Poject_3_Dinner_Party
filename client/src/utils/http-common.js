5dd7a65c9ef126527cc6a078cc3b5cc87fb235cc
Joel Delatte <joeldelatte@gmail.com>
2020-04-14 19:59:10 -0500

    update

--- client/src/utils/http-common.js
+++ client/src/utils/http-common.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
   baseURL: "http://localhost:3001",
-=======
-  baseURL: "http://rocky-meadow-16617.herokuapp.com",
->>>>>>> Develop
   headers: {
     "Content-type": "application/json"
   }
